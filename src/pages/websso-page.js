ed2e3ab6116dd4b53454d641eeab0863c0f9d48a
idmpotent2025 <venu.alla@gmail.com>
2025-08-18 04:31:09 -0600

    Update websso-page.js
    Removed ReactStrap

--- src/pages/websso-page.js
+++ src/pages/websso-page.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useLocation } from "react-router-dom";
-import { Container, Button } from "reactstrap";
+//import { Container, Button } from "reactstrap";
 //import Loading from "../components/Loading";
 
 const WebSSOPage = () => {
