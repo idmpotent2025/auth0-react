e838f7dd2010301d8b2a9dc217b10cad8c2c6c47
idmpotent2025 <venu.alla@gmail.com>
2025-08-18 04:41:52 -0600

    Update websso-page.js
    fixing errors around page layout

--- src/pages/websso-page.js
+++ src/pages/websso-page.js
@@ -35,45 +35,26 @@ export const WebSSOPage = () => {
     return <p>Redirecting to login...</p>;
   }
 
- return (
+return (
     <PageLayout>
       <div className="content-layout">
         <h1 id="page-title" className="content__title">
-          Profile Page
+          Public Page
         </h1>
         <div className="content__body">
           <p id="page-description">
             <span>
-              You can use the <strong>ID Token</strong> to get the profile
-              information of an authenticated user.
+              This page retrieves a <strong>public message</strong> from an
+              external API.
             </span>
             <span>
-              <strong>Only authenticated users can access this page.</strong>
+              <strong>Any visitor can access this page.</strong>
             </span>
           </p>
-          <div className="profile-grid">
-            <div className="profile__header">
-              <img
-                src={user.picture}
-                alt="Profile"
-                className="profile__avatar"
-              />
-              <div className="profile__headline">
-                <h2 className="profile__title">{user.name}</h2>
-                <span className="profile__description">{user.email}</span>
-              </div>
-            </div>
-            <div className="profile__details">
-              <CodeSnippet
-                title="Decoded ID Token"
-                code={JSON.stringify(user, null, 2)}
-              />
-            </div>
-          </div>
+          <CodeSnippet title="Public Message" code={message} />
         </div>
       </div>
-    </PageLayout>
-  );
+    </PageLayout> );
 };
 
 //export default WebSSOPage;
