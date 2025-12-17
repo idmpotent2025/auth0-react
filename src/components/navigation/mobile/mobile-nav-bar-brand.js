bc0833e3dc9ee9fe4a585668772375e51c717784
idmpotent2025 <venu.alla@gmail.com>
2025-12-17 03:51:35 -0700

    Update mobile-nav-bar-brand.js

--- src/components/navigation/mobile/mobile-nav-bar-brand.js
+++ src/components/navigation/mobile/mobile-nav-bar-brand.js
@@ -7,7 +7,8 @@ export const MobileNavBarBrand = ({ handleClick }) => {
       <NavLink to="/">
         <img
           className="mobile-nav-bar__logo"
-          src="https://cdn.auth0.com/blog/hub/code-samples/hello-world/auth0-logo.svg"
+          //src="https://cdn.auth0.com/blog/hub/code-samples/hello-world/auth0-logo.svg"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/01/PetSmart.svg"
           alt="Auth0 shield logo"
           width="82"
           height="24"
